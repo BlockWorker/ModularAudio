8acfbfecadf5be407a32a6b93638678f24d31b0b
BlockWorker <alex.kharitonov.2000@gmail.com>
2025-05-04 13:34:02 +0200

    DAP firmware MPU: set peripherals as device memory
    Should have really been done all along - surprised it even worked correctly in the previous state

--- firmware/DigitalAudioProcessor/Core/Src/main.c
+++ firmware/DigitalAudioProcessor/Core/Src/main.c
@@ -1035,6 +1035,16 @@ void MPU_Config(void)
   MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
 
   HAL_MPU_ConfigRegion(&MPU_InitStruct);
+
+  /** Initializes and configures the Region and the memory to be protected
+  */
+  MPU_InitStruct.Number = MPU_REGION_NUMBER4;
+  MPU_InitStruct.BaseAddress = 0x40000000;
+  MPU_InitStruct.Size = MPU_REGION_SIZE_512MB;
+  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL2;
+  MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
+
+  HAL_MPU_ConfigRegion(&MPU_InitStruct);
   /* Enables the MPU */
   HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
 
